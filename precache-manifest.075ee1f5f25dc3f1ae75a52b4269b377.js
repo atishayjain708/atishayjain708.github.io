a8ca595109cb396ca41e72a40c77a321edd5dda9
atishayjain708 <atishayjain708@gmail.com>
2019-04-04 15:07:44 +0530

    Portfolio page online

---
+++ precache-manifest.075ee1f5f25dc3f1ae75a52b4269b377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c479bebe6a7dc7969ed",
+    "url": "/portfolio/static/css/main.c3831857.chunk.css"
+  },
+  {
+    "revision": "9c479bebe6a7dc7969ed",
+    "url": "/portfolio/static/js/main.9c479beb.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/portfolio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "aa0baa22718c05ba3a262d662b9f71ff",
+    "url": "/portfolio/index.html"
+  }
+];
